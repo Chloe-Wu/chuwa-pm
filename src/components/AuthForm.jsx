f27a1d7ebf88c85e0eb93bbf8f7aaa06eb97ebf6
Yiwei <43901669+Chloe-Wu@users.noreply.github.com>
2023-12-20 16:32:08 -0800

    upload sign in page
    Create a reusable component for sign in/sign up/update password page, the data fetch part in Login.jsx file needs to be reviewed.

---
+++ src/components/AuthForm.jsx
@@ -0,0 +1,166 @@
+import { Link } from "react-router-dom";
+import styles from "../scripts/loginForm.module.css"
+import clearIcon from "../assets/clear.svg";
+import { useState } from "react";
+
+const AuthForm = ({ onAuthSubmit }) => {
+
+  const [scenario, setScenario] = useState("signup");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    onAuthSubmit({scenario,email,password})
+  };
+
+  const switchScenario = (newScenario) => {
+    setScenario(newScenario);
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <form className={styles.form} onSubmit={handleSubmit}>
+          <div className={styles.clearIconBox}>
+            <a href="https://react.dev" target="_blank">
+              <img src={clearIcon} className={styles.clearIcon} alt="clear" />
+            </a>
+          </div>
+          {scenario === "signup" && (
+            <>
+              <h2 className={styles.formTitle}>Sign up an account</h2>
+              <div className={styles.emailgroup}>
+                <label htmlFor="email">Email:</label>
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.passwordgroup}>
+                <label htmlFor="password">Password:</label>
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  className={styles.submitBtn}
+                  value="Create account"
+                />
+              </div>
+              <div className={styles.bottomArea}>
+                <div>
+                  <span>Already have an account</span>
+                  <input
+                    type="button"
+                    className={styles.underlineBtn}
+                    onClick={() => switchScenario("login")}
+                    value="login"
+                  />
+                </div>
+              </div>
+            </>
+          )}
+
+          {scenario === "login" && (
+            <>
+              <h2 className={styles.formTitle}>Sign in to your account</h2>
+              <div className={styles.emailgroup}>
+                <label htmlFor="email">Email:</label>
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div className={styles.passwordgroup}>
+                <label htmlFor="password">Password:</label>
+                <input
+                  id="password"
+                  type="password"
+                  value={password}
+                  name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  className={styles.submitBtn}
+                  value="login"
+                />
+              </div>
+              <div className={styles.bottomArea}>
+                <div>
+                  <span>Don't have an account?</span>
+                  <input
+                    type="button"
+                    className={styles.underlineBtn}
+                    onClick={() => switchScenario("signup")}
+                    value="signup"
+                  />
+                </div>
+                <input
+                  type="button"
+                  className={styles.underlineBtn}
+                  value="Forgot password?"
+                  onClick={() => switchScenario("forgot-password")}
+                />
+              </div>
+            </>
+          )}
+
+          {scenario === "forgot-password" && (
+            <>
+              <h2 className={styles.formTitle}>Update your password</h2>
+              <div className={styles.emailgroup}>
+                <label htmlFor="email">Email:</label>
+                <input
+                  id="email"
+                  type="email"
+                  value={email}
+                  name="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type="submit"
+                  className={styles.submitBtn}
+                  value="Update password"
+                  onClick={() => switchScenario("sent-email")}
+                />
+              </div>
+            </>
+          )}
+          {scenario === "sent-email" && (
+            <>
+              <p>
+                We have sent the update password link to your email, please
+                check that ！
+              </p>
+            </>
+          )}
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AuthForm;

6119ec9dd5fc7c82d5297ac6d778c11df8a0e68b
Yiwei <43901669+Chloe-Wu@users.noreply.github.com>
2024-01-12 05:48:40 -0800

    update authform component and add error component

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -1,16 +1,20 @@
 import { Link } from "react-router-dom";
-import styles from "../scripts/loginForm.module.css";
-import clearIcon from "../assets/clear.svg";
+import styles from "./loginForm.module.css";
+import clearIcon from "../../assets/clear.svg";
 import { useState } from "react";
+import { PiEye, PiEyeClosed } from "react-icons/pi";
 
-const AuthForm = ({ onAuthSubmit }) => {
-  const [scenario, setScenario] = useState("signup");
+const AuthForm = ({ onAuthSubmit,scenario: initialScenario }) => {
+  const [scenario, setScenario] = useState(initialScenario);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [admin, setAdmin] = useState(false);
+  const[showpassword,setShowPassword] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    
     onAuthSubmit({ scenario, email, password, admin });
   };
 
@@ -18,14 +22,18 @@ const AuthForm = ({ onAuthSubmit }) => {
     setScenario(newScenario);
   };
 
+  const handlePasswordClick = () =>{
+    setShowPassword(!showpassword);
+  }
+
   return (
     <>
       <div className={styles.logcontainer}>
         <form className={styles.form} onSubmit={handleSubmit}>
           <div className={styles.clearIconBox}>
-            <a href="https://react.dev" target="_blank">
+            <Link to="/login">
               <img src={clearIcon} className={styles.clearIcon} alt="clear" />
-            </a>
+            </Link>
           </div>
           {scenario === "signup" && (
             <>
@@ -43,41 +51,47 @@ const AuthForm = ({ onAuthSubmit }) => {
               </div>
               <div className={styles.passwordgroup}>
                 <label htmlFor="password">Password:</label>
-                <input
-                  id="password"
-                  type="password"
-                  value={password}
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
+                <div className={styles.passwordField}>
+                  <input
+                    id="password"
+                    type={showpassword ? "text" : "password"}
+                    value={password}
+                    name="password"
+                    className={styles.passwordInput}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <div className={styles.showPasswordIcon} onClick={handlePasswordClick}>{showpassword?<PiEye size={25} />:<PiEyeClosed  size={25}/>}</div>
+                </div>
               </div>
               <div className={styles.adminBox}>
                 <label htmlFor="admin">Administrator:</label>
                 <input
                   id="admin"
                   type="checkbox"
+                  className={styles.checkbox}
                   value={admin}
                   name="admin"
                   onChange={(e) => setAdmin(e.target.checked)}
                 />
               </div>
-              <div className={styles.submitBtn}>
+              <div>
                 <input
                   type="submit"
-        
                   value="Create account"
+                  className={styles.submitBtn}
                 />
               </div>
               <div className={styles.bottomArea}>
                 <div className={styles.bottomLeftArea}>
                   <span>Already have an account</span>
-                  <input
+                  {/* <input
                     type="button"
                     className={styles.underlineBtn}
                     onClick={() => switchScenario("login")}
                     value="login"
-                  />
+                  /> */}
+                  <Link to="/login">login</Link>
                   </div>
               </div>
             </>
@@ -99,37 +113,34 @@ const AuthForm = ({ onAuthSubmit }) => {
               </div>
               <div className={styles.passwordgroup}>
                 <label htmlFor="password">Password:</label>
-                <input
-                  id="password"
-                  type="password"
-                  value={password}
-                  name="password"
-                  onChange={(e) => setPassword(e.target.value)}
-                  required
-                />
+                <div className={styles.passwordField}>
+                  <input
+                    id="password"
+                    type={showpassword ? "text" : "password"}
+                    value={password}
+                    name="password"
+                    className={styles.passwordInput}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                  <div className={styles.showPasswordIcon} onClick={handlePasswordClick}>{showpassword?<PiEye size={25} />:<PiEyeClosed  size={25}/>}</div>
+                </div>
               </div>
-              <div className={styles.submitBtn}>
+              <div>
                 <input
                   type="submit"
-                  value="login"
+                  value="Sign In"
+                  className={styles.submitBtn}
                 />
               </div>
               <div className={styles.bottomArea}>
                 <div>
-                  <span>Don't have an account?</span>
-                  <input
-                    type="button"
-                    className={styles.underlineBtn}
-                    onClick={() => switchScenario("signup")}
-                    value="signup"
-                  />
+                  <span>Don't have an account? </span>
+
+                  <Link to="/signup">signup</Link>
                 </div>
-                <input
-                  type="button"
-                  className={styles.underlineBtn}
-                  value="Forgot password?"
-                  onClick={() => switchScenario("forgot-password")}
-                />
+                <Link to="/updatePassword">Forgot password?</Link>
+
               </div>
             </>
           )}
@@ -137,6 +148,7 @@ const AuthForm = ({ onAuthSubmit }) => {
           {scenario === "forgot-password" && (
             <>
               <h2 className={styles.formTitle}>Update your password</h2>
+              <span>Enter your email link, we will send you the recovery link</span>
               <div className={styles.emailgroup}>
                 <label htmlFor="email">Email:</label>
                 <input
@@ -148,10 +160,11 @@ const AuthForm = ({ onAuthSubmit }) => {
                   required
                 />
               </div>
-              <div className={styles.submitBtn}>
+              <div>
                 <input
                   type="submit"
                   value="Update password"
+                  className={styles.submitBtn}
                   onClick={() => switchScenario("sent-email")}
                 />
               </div>

d544261d737eb73eba7d11212d32dc0230d3fa79
Yiwei <43901669+Chloe-Wu@users.noreply.github.com>
2024-01-15 11:30:35 -0800

    embed header/footer into signup/in pages

--- src/pages/UpdatePassword.jsx
+++ src/pages/UpdatePassword.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import AuthForm from "../components/AuthForm/AuthForm";
 import { useNavigate } from 'react-router-dom';
 
+import Header from '../components/Framework/Header';
+import Footer from "../components/Framework/Footer";
+
 
 export default function UpdatePassword() {
   return (
     <div>
+      <Header/>
       <AuthForm scenario='forgot-password'/>
+      <Footer/>
     </div>
   );
 }
